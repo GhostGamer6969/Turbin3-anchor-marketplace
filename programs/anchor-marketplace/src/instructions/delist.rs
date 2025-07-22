ba520223bb2ccaeae4e132bddd3119edb61ad36a
GhostGamer6969 <arambhmani@hotmail.com>
2025-07-22 20:14:13 +0530

    delist fix

--- programs/anchor-marketplace/src/instructions/delist.rs
+++ programs/anchor-marketplace/src/instructions/delist.rs
@@ -1,8 +1,8 @@
 use anchor_lang::prelude::*;
 use anchor_spl::{
-    token::{close_account, CloseAccount},
     associated_token::AssociatedToken,
-    token_interface::{TokenInterface, Mint, TokenAccount, TransferChecked, transfer_checked},
+    token::{close_account, CloseAccount},
+    token_interface::{transfer_checked, Mint, TokenAccount, TokenInterface, TransferChecked},
 };
 
 use crate::state::{Listing, Marketplace};
@@ -49,52 +49,49 @@ pub struct Delist<'info> {
     pub system_program: Program<'info, System>,
 }
 
-impl <'info> Delist <'info> {
+impl<'info> Delist<'info> {
+    pub fn refund_nft(&mut self) -> Result<()> {
+        let cpi_program = self.token_program.to_account_info();
 
-        pub fn refund_nft (&mut self) -> Result<()> {
+        let cpi_accounts = TransferChecked {
+            from: self.vault.to_account_info(),
+            mint: self.maker_mint.to_account_info(),
+            to: self.maker_ata.to_account_info(),
+            authority: self.listing.to_account_info(),
+        };
 
-            let cpi_program = self.token_program.to_account_info();
-
-            let cpi_accounts = TransferChecked{
-                from: self.vault.to_account_info(),
-                mint: self.maker_mint.to_account_info(),
-                to: self.maker_ata.to_account_info(),
-                authority: self.listing.to_account_info()
-            };
-
-            let seeds: &[&[u8]; 3] = &[
+        let seeds: &[&[u8]; 3] = &[
             &self.marketplace.key().to_bytes()[..],
             &self.maker_mint.key().to_bytes()[..],
             &[self.listing.bump],
-            ];
+        ];
 
-            let signer_seeds= &[&seeds[..]];
+        let signer_seeds = &[&seeds[..]];
 
-            let ctx = CpiContext::new_with_signer(cpi_program,cpi_accounts,signer_seeds);
+        let ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, signer_seeds);
 
-            transfer_checked(ctx, self.maker_ata.amount, self.maker_mint.decimals)
-        }
+        transfer_checked(ctx, self.maker_ata.amount, self.maker_mint.decimals)
+    }
 
-        pub fn close_mint_vault(&mut self) -> Result<()> {
-            let seeds: &[&[u8]; 3] = &[
-                &self.marketplace.key().to_bytes()[..],
-                &self.maker_mint.key().to_bytes()[..],
-                &[self.listing.bump],
-            ];
+    pub fn close_mint_vault(&mut self) -> Result<()> {
+        let seeds: &[&[u8]; 3] = &[
+            &self.marketplace.key().to_bytes()[..],
+            &self.maker_mint.key().to_bytes()[..],
+            &[self.listing.bump],
+        ];
 
-            let signer_seeds= &[&seeds[..]];
+        let signer_seeds = &[&seeds[..]];
 
-            let cpi_program = self.token_program.to_account_info();
+        let cpi_program = self.token_program.to_account_info();
 
-            let close_accounts = CloseAccount {
-                account: self.vault.to_account_info(),
-                destination: self.maker.to_account_info(),
-                authority: self.listing.to_account_info(),
-            };
+        let close_accounts = CloseAccount {
+            account: self.vault.to_account_info(),
+            destination: self.maker.to_account_info(),
+            authority: self.listing.to_account_info(),
+        };
 
-            let cpi_ctx = CpiContext::new_with_signer(cpi_program, close_accounts, signer_seeds);
+        let cpi_ctx = CpiContext::new_with_signer(cpi_program, close_accounts, signer_seeds);
 
-            close_account(cpi_ctx)
-        }
-
+        close_account(cpi_ctx)
+    }
 }
