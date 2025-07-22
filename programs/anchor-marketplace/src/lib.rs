c9a22c75a3ab74312e6b18f8e5d471fac6fa16a7
GhostGamer6969 <arambhmani@hotmail.com>
2025-07-22 20:28:29 +0530

    lib rs

--- programs/anchor-marketplace/src/lib.rs
+++ programs/anchor-marketplace/src/lib.rs
@@ -1,3 +1,5 @@
+#![allow(deprecated)]
+#![allow(unexpected_cfgs)]
 pub mod constants;
 pub mod error;
 pub mod instructions;
@@ -15,7 +17,20 @@ declare_id!("5Afut8Xsyhwt1Lxy4CHEMqMawdm7Tdoy8cwCLET2qKyX");
 pub mod anchor_marketplace {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        initialize::handler(ctx)
+    pub fn initialize(ctx: Context<Initialize>, name: String, fee: u16) -> Result<()> {
+        ctx.accounts.init(name, fee, &ctx.bumps)
+    }
+
+    pub fn list(ctx: Context<List>, price: u64) -> Result<()> {
+        ctx.accounts.create_listing(price, &ctx.bumps)?;
+        ctx.accounts.deposit_nft()
+    }
+    pub fn delist(ctx: Context<Delist>) -> Result<()> {
+        ctx.accounts.refund_nft()?;
+        ctx.accounts.close_mint_vault()
+    }
+
+    pub fn purchase(ctx: Context<Purchase>) -> Result<()> {
+        ctx.accounts.purchase()
     }
 }
